042303d1c6f5ce92bb86ddd9bea1468236848c76
ej3131 <eunnnn31@gmail.com>
2025-08-09 16:14:14 +0900

    Player 클래스

---
+++ Player.java
@@ -0,0 +1,22 @@
+package project_2;
+
+// 플레이어의 상태 추적, 아이템 사용 행위 제어
+
+public class Player {
+
+    public void moveTo(Room room){ //빈칸 채워야 함(Room)
+
+    }
+
+    public void useItem(String item) { //빈칸 채우기
+
+    }
+
+    public void die() {
+
+    }
+
+    public void solvePuzzle(Puzzle puzzle) { //빈칸 채우기
+
+    }
+}
